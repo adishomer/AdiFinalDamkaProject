0b553251ff13ed00f0a113079a3ae0136a56d68a
User <adishomer123@gmail.com>
2026-02-05 13:32:45 +0200

    Setting

--- app/src/main/java/com/example/damkaprojectadi/MainActivity.java
+++ app/src/main/java/com/example/damkaprojectadi/MainActivity.java
@@ -1,11 +1,18 @@
 package com.example.damkaprojectadi;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
@@ -13,6 +20,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private Button btnPlay;
     private Button btnInstructions;
     private Button btnSetting;
+    private ActivityResultLauncher<Intent> activityResultLauncher;
+    private String backgroundColor = ""; // שמירת צבע הרקע
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +36,23 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         btnPlay.setOnClickListener(this);
         btnInstructions.setOnClickListener(this);
         btnSetting.setOnClickListener(this);
+
+        activityResultLauncher = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                new ActivityResultCallback<ActivityResult>() {
+                    @Override
+                    public void onActivityResult(ActivityResult result) {
+                        if (result.getResultCode() == RESULT_OK) {
+                            // There are no request codes
+                            Intent data = result.getData();
+                            String str = data.getStringExtra("color");
+                            Toast.makeText(MainActivity.this, ""+str, Toast.LENGTH_SHORT).show();
+                            setBackgroundColor(str);
+
+                        }
+                    }
+                }
+        );
 
     }
 
@@ -45,7 +71,38 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         if (view==btnSetting)
         {
             Intent intent= new Intent(this, Setting.class);
-            startActivity(intent);
+            activityResultLauncher.launch(intent);
+        }
+
+    }
+
+    public void setBackgroundColor(String strColor) {
+        // פונקציה זו מקבלת את צבע הרקע
+        // שומרת אותו בתכונה backgroundColor
+        // ומשנה את צבע הרקע של המסך
+
+        // if Firebase empty srtColor == null
+        if(strColor == null)
+            strColor = "";
+
+        backgroundColor = strColor;
+        LinearLayout linearLayout = findViewById(R.id.activity_main);
+        switch (strColor)
+        {
+            case "Red":
+                linearLayout.setBackgroundColor(Color.RED); // 0x = hexadecimal
+                break;
+
+            case "Blue":
+                linearLayout.setBackgroundColor(Color.BLUE);
+                break;
+
+            case "Pink":
+                linearLayout.setBackgroundColor(Color.argb(255,255,192,203)); //Alfa Red Green Blue
+                break;
+            default:
+                linearLayout.setBackgroundColor(Color.YELLOW);
+                break;
         }
 
     }
--- app/src/main/java/com/example/damkaprojectadi/Setting.java
+++ app/src/main/java/com/example/damkaprojectadi/Setting.java
@@ -3,14 +3,23 @@ package com.example.damkaprojectadi;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class Setting extends AppCompatActivity implements View.OnClickListener {
+public class Setting extends AppCompatActivity implements View.OnClickListener, AdapterView.OnItemSelectedListener {
+
 
     private Button btns;
+    String[] arrcolor = { "בחר צבע","Red", "Blue", "Pink", "Yellow" };
+    Spinner spinner;
+    boolean firstTime = true;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,6 +29,13 @@ public class Setting extends AppCompatActivity implements View.OnClickListener {
         btns = findViewById(R.id.btnSet);
         btns.setOnClickListener(this);
 
+        spinner = findViewById(R.id.spinner);
+        spinner.setOnItemSelectedListener(this);
+
+        ArrayAdapter aa = new ArrayAdapter(this,android.R.layout.simple_spinner_item,arrcolor);
+        aa.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(aa); //Setting the ArrayAdapter data on the Spinner
+
     }
 
     @Override
@@ -28,5 +44,23 @@ public class Setting extends AppCompatActivity implements View.OnClickListener {
             Intent intent = new Intent(this, MainActivity.class);
             startActivity(intent);
         }
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        if(!firstTime)
+        {
+            Intent intent = new Intent();
+            intent.putExtra("color",arrcolor[position]);
+            setResult(RESULT_OK, intent);
+            finish(); // return to MainActivity
+        }
+        firstTime = false;
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+        Toast.makeText(this, "onNothingSelected", Toast.LENGTH_SHORT).show();
+
     }
 }
